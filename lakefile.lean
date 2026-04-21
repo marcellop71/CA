2c0fc6b69195a5c668d7029167d9c44dbd966ad2
Marcello Paris <marcello.paris@gmail.com>
2026-04-21 07:28:43 +0200

    minor

--- lakefile.lean
+++ lakefile.lean
@@ -5,10 +5,10 @@ package "ca" where
   moreLinkArgs := #["-Wl,--allow-shlib-undefined", "-lssl", "-lcrypto"]
 
 require batteries from git
-  "https://github.com/leanprover-community/batteries" @ "v4.29.0-rc1"
+  "https://github.com/leanprover-community/batteries" @ "v4.29.0"
 
 require Cli from git
-  "https://github.com/leanprover/lean4-cli" @ "main"
+  "https://github.com/leanprover/lean4-cli" @ "v4.29.0"
 
 require redisLean from git
   "https://github.com/marcellop71/redis-lean" @ "main"
